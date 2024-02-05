faf21db39f3328769978c53818381189069a9514
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-02-05 20:52:31 +0000

    Add feature to get index of array

--- hashmap.js
+++ hashmap.js
@@ -1,4 +1,4 @@
-import { Bucket, Node } from "./bucket.js";
+import { Bucket } from "./bucket.js";
 
 class HashMap {
   constructor() {
@@ -86,14 +86,6 @@ class HashMap {
     for (let i = 0; i < this.array.length; i++) {
       this.array[i] = new Bucket();
     }
-    // this.array.forEach((bucket) => {
-    //   bucket = new Node();
-    // });
-    // this.array.forEach((bucket) => {
-    //   if (this.keys().includes(bucket.key)) {
-    //     this.remove(bucket.key);
-    //   }
-    // });
   }
   entries() {
     const totalEntriesArray = [];
@@ -136,70 +128,3 @@ myHashMap.set("Harper", "Miller");
 myHashMap.set("Noah", "Jones");
 
 console.log(myHashMap.entries());
-
-// if (index < 0 || index >= buckets.length) {
-//   throw new Error("Trying to access index out of bound");
-// }
-
-// class HashMap {
-//   constructor() {
-//     this.buckets = [
-//       new Bucket(1, "how are you"),
-//       new Bucket("2", "im good bro"),
-//       new Bucket(),
-//       new Bucket("three", "how about you"),
-//       new Bucket(),
-//     ];
-//   }
-//   get(key) {
-//     let bucketValue;
-//     this.buckets.forEach((bucket) => {
-//       if (bucket.key === key) {
-//         bucketValue = bucket.value;
-//       }
-//     });
-//     return bucketValue ?? null;
-//   }
-//   changeValue(key, value) {
-//     this.buckets.forEach((bucket) => {
-//       if (bucket.key === key) {
-//         bucket.value = value;
-//       }
-//     });
-//   }
-//   set(index, key, value) {
-//     if (index < 0 || index >= this.buckets.length) {
-//       throw new Error("Trying to access index out of bound");
-//     }
-//     this.buckets[index] = new Bucket(key, value);
-//     if (this.shouldGrowBuckets()) this.buckets.push(new Bucket());
-//   }
-//   shouldGrowBuckets() {
-//     const loadFactor = 0.75;
-//     let counter = 0;
-
-//     this.buckets.forEach((bucket) => {
-//       if (bucket.key !== undefined) counter++;
-//     });
-//     return counter / this.buckets.length > loadFactor;
-//   }
-//   // hash(key) {
-//   //   let hashCode = 0;
-
-//   //   const primeNumber = 31;
-//   //   for (let i = 0; i < key.length; i++) {
-//   //     hashCode = primeNumber * hashCode + key.charCodeAt(i);
-//   //   }
-
-//   //   return hashCode;
-//   // }
-// }
-
-// class Bucket {
-//   constructor(key, value) {
-//     this.key = key;
-//     this.value = value;
-//   }
-// }
-
-// const hash = new HashMap();
